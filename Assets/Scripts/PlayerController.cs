2c1d6604bdbd387ced31cdc700dcb33f6eb1dd67
agdodson <andy.dodson@outlook.com>
2020-10-06 15:35:12 +0300

    - Setup and UserInput Challenge

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1.0f;
+    
+ 
+    void Start()
+    {
+        // take current positon = new postion (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get vertical and horizontal Inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        // calcutale speeds
+        float verticalSpeed = 1 * verticalInput * _speed * Time.deltaTime;
+        float horizontalSpeed = 1 * horizontalInput * _speed * Time.deltaTime;
+        transform.Translate(horizontalSpeed,verticalSpeed,0);
+    }
+}
