3c8874ed5af91db72dde2ecf28ff69bc43b878c8
agdodson <andy.dodson@outlook.com>
2020-10-06 17:59:42 +0300

    - Destroy laser when out of bounds

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
 	[SerializeField] private float _speed = 8.0f;
+	[SerializeField] private float _upperBound = 8.0f;
 
 	// Start is called before the first frame update
 	void Start()
@@ -17,5 +18,11 @@ public class Laser : MonoBehaviour
 	void Update()
 	{ 
 		transform.Translate(Vector3.up * _speed * Time.deltaTime, 0);
+
+		// Destroy laser when out of bounds
+		if (transform.position.y > _upperBound)
+		{
+			Destroy(gameObject);
+		}
 	}
 }
