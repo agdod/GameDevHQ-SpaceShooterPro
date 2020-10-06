f1f2fc4deda73b026c0da9655b6046e78baffa7d
agdodson <andy.dodson@outlook.com>
2020-10-06 17:18:32 +0300

    - Adding player Bounds , code cleanup (if statement to Clamp on y axis)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,52 +6,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float _speed = 1.0f;
-
+	[SerializeField] private float _yLowerBound = -2.0f;
+	[SerializeField] private float _yUpperBound = 0.0f;
 
 	void Start()
 	{
-		// take current positon = new postion (0,0,0)
-		transform.position = new Vector3(0, 0, 0);
+		// Set player postion to zero
+		transform.position = Vector3.zero;
 	}
 
-	// Update is called once per frame
 	void Update()
+	{
+		PlayerMovement();
+	}
+
+	void PlayerMovement()
 	{
 		// Get vertical and horizontal Inputs
 		float horizontalInput = Input.GetAxis("Horizontal");
 		float verticalInput = Input.GetAxis("Vertical");
-		// calcutale speeds
-		//float verticalSpeed = 1 * verticalInput * _speed * Time.deltaTime;
-		//float horizontalSpeed = 1 * horizontalInput * _speed * Time.deltaTime;
-		//Clean up code - (*_speed* Time.deltaime is commmon to both )
+
+		// calcutale speed and direction
 		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 		transform.Translate(direction * _speed * Time.deltaTime);
 
-		// Limit player bounds
-		/*	if player position on the y axis is greater than 0
-		 *		y pos = 0
-		 *	else if position on the y is less than -3.8f (only use else if if checking same value ie y)
-		 *		y pos = -3.8f
+		// Limit player bounds on y and x axis
+		//Clamp on y axis
+		transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _yLowerBound, _yUpperBound), 0);
 
-		 *	if player on the x > 11
-		 *		x pos = -11
-		 *	else if player on the x is less than -11
-		 *	x pos = 11
-		 */
-		//bound on y axis
-		if (transform.position.y >= 0)
-		{
-			transform.position = new Vector3(transform.position.x, 0, 0);
-		} 
-		else if (transform.position.y <= -2.0f)
-		{
-			transform.position = new Vector3(transform.position.x, -2.0f, 0);
-		}
 		// bound on x axis
 		if (transform.position.x > 11)
 		{
 			transform.position = new Vector3(-11, transform.position.y, 0);
-		} 
+		}
 		else if (transform.position.x <= -11)
 		{
 			transform.position = new Vector3(11, transform.position.y, 0);
