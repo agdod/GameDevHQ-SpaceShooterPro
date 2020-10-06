11dc81751c727ac9329bee3b8f8024baadc2f7ea
agdodson <andy.dodson@outlook.com>
2020-10-06 16:38:56 +0300

    - Challenge Limit Player Bounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,5 +26,35 @@ public class PlayerController : MonoBehaviour
 		//Clean up code - (*_speed* Time.deltaime is commmon to both )
 		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 		transform.Translate(direction * _speed * Time.deltaTime);
+
+		// Limit player bounds
+		/*	if player position on the y axis is greater than 0
+		 *		y pos = 0
+		 *	else if position on the y is less than -3.8f (only use else if if checking same value ie y)
+		 *		y pos = -3.8f
+
+		 *	if player on the x > 11
+		 *		x pos = -11
+		 *	else if player on the x is less than -11
+		 *	x pos = 11
+		 */
+		//bound on y axis
+		if (transform.position.y >= 0)
+		{
+			transform.position = new Vector3(transform.position.x, 0, 0);
+		} 
+		else if (transform.position.y <= -2.0f)
+		{
+			transform.position = new Vector3(transform.position.x, -2.0f, 0);
+		}
+		// bound on x axis
+		if (transform.position.x > 11)
+		{
+			transform.position = new Vector3(-11, transform.position.y, 0);
+		} 
+		else if (transform.position.x <= -11)
+		{
+			transform.position = new Vector3(11, transform.position.y, 0);
+		}
 	}
 }
