a9738a9cdf791055d702a0679b9ae5b58b1e7618
agdodson <andy.dodson@outlook.com>
2020-10-08 12:18:29 +0300

    - Added basic PowerUp beahaviour - made collectable

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    /* Move down at defined speed
+     * When leave screen, destroy
+     * OnTriggerCollsion
+     *      Only collecable by player
+     *      on collected destroy
+     */
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _lowerBounds = -3.5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y < _lowerBounds)
+		{
+            Destroy(gameObject);
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+            //activate powerup
+            Destroy(gameObject);
+		}
+	}
+}
