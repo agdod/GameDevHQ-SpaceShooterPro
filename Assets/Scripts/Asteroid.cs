a339050cf0360327929e050debf28e2d3eb24669
agdodson <andy.dodson@outlook.com>
2020-10-10 15:11:25 +0300

    - Added Asteroid and adpplied spin rate.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float _spinRate = 0.65f;
+	private Vector3 _rotationRate;
+
+	private void Start()
+	{
+		_rotationRate = new Vector3(0, 0, _spinRate);
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		transform.Rotate(_rotationRate);
+	}
+}
