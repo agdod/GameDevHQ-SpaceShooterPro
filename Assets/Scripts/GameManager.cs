bff6804bbbbdc70d5295a7f0b79b984f4c2d982b
agdodson <andy.dodson@outlook.com>
2020-10-12 12:30:13 +0300

    - Added quit and return to main menu function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,25 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private bool _isGameOver;
+	[SerializeField] private bool _isGameOver;
 
-    void Start()
-    {
-        _isGameOver = false;
-    }
-
-   
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
-        {
-            // Reload scene to restart
-            SceneManager.LoadScene("Game");
-        }
-    }
-
-    public void SetGameOver()
+	void Start()
 	{
-        _isGameOver = true;
+		_isGameOver = false;
+	}
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
+		{
+			// Reload scene to restart
+			SceneManager.LoadScene("Game");
+		}
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		if (Input.GetKeyDown(KeyCode.Backspace))
+		{
+			SceneManager.LoadScene("Main_Menu");
+		}
+	}
+
+	public void SetGameOver()
+	{
+		_isGameOver = true;
 	}
 }
